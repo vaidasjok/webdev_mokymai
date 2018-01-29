5fe26b13f80d3ed6293d34508bfa8ee15b5bfbf0
Vaidas <vaidasjok@gmail.com>
2018-01-29 17:42:33 +0200

    13 pamoka - pradzia

---
+++ pamoka13/assets/js/pamoka.js
@@ -0,0 +1,20 @@
+// window.onload="initFunctions()";
+$( document ).ready(function() {
+    initFunctions();
+});
+
+function initFunctions() 
+{
+	name_transfer();
+
+	
+}
+
+function someFunction() 
+		{
+			
+
+		}
+
+	
+		
