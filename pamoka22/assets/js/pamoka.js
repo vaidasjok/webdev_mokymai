bc4abeab9a2e4e97c8c0e7697e3ebbd939d52246
Vaidas <vaidasjok@gmail.com>
2018-02-23 18:22:31 +0200

    22 pamokos ikelimas

---
+++ pamoka22/assets/js/pamoka.js
@@ -0,0 +1,59 @@
+// var user = {
+// 	name: 'Vaida',
+// 	lastName: 'VaidaVaida',
+// 	country: 'Lithuania',
+// 	getAddress: function(street, city, number) {
+// 		alert(street + ' ' + number + ', ' + city + ', ' + this.country);
+// 	},
+// 	setName: function(name) {
+// 		this.name = name;
+// 	},
+// 	getName: function() {
+// 		return this.name;
+// 	}
+// }
+
+// var user = {
+// 	name: 'Vaida',
+// 	lastName: 'VaidaVaida',
+// 	country: 'Lithuania',
+// 	getAddress(street, city, number) {
+// 		alert(street + ' ' + number + ', ' + city + ', ' + this.country);
+// 	},
+// 	setName(name) {
+// 		this.name = name;
+// 	},
+// 	getName() {
+// 		return this.name;
+// 	}
+// }
+
+// var user = {
+// 	name: 'Vaida',
+// 	lastName: 'VaidaVaida',
+// 	userCountry: 'Lithuania',
+// 	getAddress: (street, city, number) => {
+// 		alert(street + ' ' + number + ', ' + city + ', ' + country);
+// 	},
+// 	setName: (name) => {
+		
+// 		this.name = name;
+// 	},
+// 	getName: () => {
+// 		return this.name;
+// 	}
+// }
+
+
+
+// user.getAddress('Laisves', 'Vilnius', 12);
+// user.setName('Vaidute');
+// alert(user.getName());
+// alert(user.name);
+
+
+
+
+
+
+
---
+++ pamoka22/layouts/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title></title>
+	<meta description="fasfdfasd">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/stilius.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+	  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	  crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/6.1.19/browser.min.js"></script>
+	<script src="assets/js/pamoka.js"></script>
+
+
+</head>
